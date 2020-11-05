5dde55d33f7c9e98d7a745f8968198e5ff8c8f9c
MarcB98 <marcbatalla652@gmail.com>
2020-11-05 10:02:47 +0100

    Falla el insert del registro

--- AltaClientes/registro-ok.php
+++ AltaClientes/registro-ok.php
@@ -21,6 +21,7 @@ function alert($message) {
 
 //REALIZAMOS LA CONEXIÓN
 $con=mysqli_connect($servidor,$user,$pass,$bd);
+
 if(!$con) {
 	die(alert("Error al realizar la conexión: ". mysqli_connect_error() . "<br>"));
 } else {
@@ -28,7 +29,7 @@ if(!$con) {
 }
 
 //CREAMOS OBJETO USUARIO Y LO INICIALIZAMOS
-$usuario = new Usuario();
+$usuario = new Clientes();
 $usuario -> constructor($_GET["usuario"], $_GET["contra"], $_GET["nombre"], $_GET["apellido"], $_GET["email"], $_GET["edad"], $_GET["sexo"], $_GET["dni"]);
 
 //LLAMAMOS A LAS COMPOVACIONES DEL ARCHIVO usuario.php
@@ -36,5 +37,15 @@ $usuario -> validarPassword($_GET["contra"]);
 $usuario -> validarDNI($_GET["dni"]);
 $usuario -> validarNombreApellido($_GET["nombre"],$_GET["apellido"]);
 $usuario -> validarCorreo($_GET["email"]);
+
+//INSERT DE LOS DATOS A LA BBDD
+$consulta = mysql_query($con,"INSERT INTO 'clientes' ( `usuario` , `pass` , `nombre` , `apellidos` , `email` , `edad` , `sexo` , `dni` , `id_usuario` , `tipo` )VALUES ( '".$usuario -> getUsuario()."' , '".$usuario -> getContrasena()."' , '".$usuario -> getNombre()."' , '".$usuario -> getApellido()."' , '".$usuario -> getEmail()."' , '".$usuario -> getEdad()."' , '".$usuario -> getSexo()."' , '".$usuario -> getDni()."' )");
+
+if(!$consulta) {
+	die(alert("¡¡Error en la consulta!! "));
+} else {
+	alert("Registro correcto!!");
+	header("Location: ../index.html");
+}
 
 ?>
