fd362de955d4235e5294df2d7f2a65e05f7ebe6f
MarcB98 <marcbatalla652@gmail.com>
2020-12-29 19:04:49 +0100

    Commit Tutorial video nº22 fet

--- PW_php/controllers/UserController.php
+++ PW_php/controllers/UserController.php
@@ -14,16 +14,27 @@ class UserController extends Controllers
     public function Registro()
     {
 
-        if (isset($_SESSION['model1'])) {
+        if (isset($_SESSION['model1']) && isset($_SESSION['model2'])) {
+
             $array1 = unserialize($_SESSION['model1']);
-            if ($array1 != null) {
+            $array2 = unserialize($_SESSION['model2']);
+
+            if ($array1 != null && $array2 != null) {
+
                 $model1 = $this->TUser($array1);
-                $this->view->Render($this, "registro", $model1, null, null);
+                $model2 = $this->TUser($array2);
+                $this->view->Render($this, "registro", $model1, $model2, null);
+
             }else {
+
                 $this->view->Render($this, "registro", null, null, null);
+
             }
+
         }else {
+
             $this->view->Render($this, "registro", null, null, null);
+
         }
         
     }
@@ -39,6 +50,57 @@ class UserController extends Controllers
             $execute = false;
         }
 
+        if (empty($_POST["nombre"])) {
+
+            $nombre = "Ingrese un Nombre";
+            $execute = false;
+        }
+
+        if (empty($_POST["apellido"])) {
+
+            $apellido = "Ingrese un Apellido";
+            $execute = false;
+        }
+
+        if (empty($_POST["email"])) {
+
+            $email = "Ingrese un Correo Electronico";
+            $execute = false;
+        }else {
+            
+            if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+                
+                $email = "Ingrese un Correo Electronico Valido!";
+                $execute = false;
+
+            }
+
+        }
+
+        if (empty($_POST["user"])) {
+
+            $user = "Ingrese un Usuario";
+            $execute = false;
+        }
+
+        if (empty($_POST["pass"])) {
+
+            $pass = "Ingrese una Contraseña";
+            $execute = false;
+        }
+
+        if (empty($_POST["repass"])) {
+
+            $repass = "Campo Obligatorio";
+            $execute = false;
+
+        }
+
+        if ($_POST["repass"] != $_POST["pass"]) {
+            $repass = "Las contraseñas no coinciden";
+            $execute = false;
+        }
+
         $_SESSION['model1'] = serialize(array(
             $_POST["dni"],
             $_POST["nombre"],
@@ -47,6 +109,16 @@ class UserController extends Controllers
             $_POST["user"],
             $_POST["pass"],
             $_POST["repass"],
+        ));
+
+        $_SESSION['model2'] = serialize(array(
+            $dni,
+            $nombre,
+            $apellido,
+            $email,
+            $user,
+            $pass,
+            $repass,
         ));
 
         header('Location: registro');
